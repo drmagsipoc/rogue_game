7a0e2b15012e7788649c4e16064e6771078ddf0c
David Rigel Magsipoc <drmagsipoc@gmail.com>
2019-04-20 23:39:45 +0800

    added fighter structure

--- src/main.rs
+++ src/main.rs
@@ -112,6 +112,18 @@ fn make_map(objects: &mut Vec<Object>) -> Map {
     map
 }
 
+/// combat-related properties and methods for monster, player, NPC).
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Fighter {
+    max_hp: i32,
+    hp: i32,
+    defense: i32,
+    power: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Ai;
+
 // Generic object: Player, Monster, Item, Stairs
 struct Object {
     name: String,
@@ -121,6 +133,8 @@ struct Object {
     y: i32,
     char: char,
     color: Color,
+    fighter: Option<Fighter>,
+    ai: Option<Ai>,
 }
 
 impl Object {
@@ -142,6 +156,8 @@ impl Object {
             color: color,
             blocks: blocks,
             alive: false,
+            fighter: None,
+            ai: None,
         }
     }
 
@@ -200,9 +216,16 @@ fn place_objects(room: Rect, map: &Map, objects: &mut Vec<Object>) {
             // generate the monsters
             let mut monster = if rand::random::<f32>() < 0.8 {  // 80% chance to create an orc
                 // create orc
-                Object::new(x, y, 'o', "orc", colors::DESATURATED_GREEN, true)
+                let mut orc = Object::new(x, y, 'o', "orc", colors::DESATURATED_GREEN, true);
+                orc.fighter = Some(Fighter{max_hp: 4, hp: 4, defense: 0, power: 1});
+                orc.ai = Some(Ai);
+                orc
             } else {
-                Object::new(x, y, 'T', "troll", colors::DARKER_GREEN, true)
+                // create troll
+                let mut troll = Object::new(x, y, 'T', "troll", colors::DARKER_GREEN, true);
+                troll.fighter = Some(Fighter{max_hp: 5, hp: 3, defense: 1, power: 1});
+                troll.ai = Some(Ai);
+                troll
             };
             monster.alive = true;
             objects.push(monster);
@@ -360,6 +383,7 @@ fn main() {
 
     // creation of objects
     let mut player = Object::new(0, 0, '@', "player", colors::WHITE, true);
+    player.fighter = Some(Fighter{max_hp: 5, hp: 5, defense: 1, power: 2});
     player.alive = true;
 
     // objects list
