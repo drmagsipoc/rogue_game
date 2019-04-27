4f8ef95df8049538ac66bdb142ae5a671fa66dd5
David Rigel Magsipoc <drmagsipoc@gmail.com>
2019-04-27 14:14:41 +0800

    added loading and saving functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 extern crate tcod;
 extern crate rand;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
 
+// the existing imports
+use std::io::{Read, Write};
+use std::fs::File;
+use std::error::Error;
 use std::cmp;
 use tcod::console::*;
 use tcod::colors::{self, Color};
@@ -52,13 +59,14 @@ struct Tcod {
     mouse: Mouse,
 }
 
+#[derive(Serialize, Deserialize)]
 struct Game {
     map: Map,
     log: Messages,
     inventory: Vec<Object>,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 enum Item {
     Heal,
     Lightning,
@@ -240,7 +248,7 @@ fn mut_two<T>(first_index: usize, second_index: usize, items: &mut [T]) -> (&mut
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 struct Tile {
     blocked: bool,
     explored: bool,
@@ -319,7 +327,7 @@ fn make_map(objects: &mut Vec<Object>) -> Map {
 }
 
 /// combat-related properties and methods for monster, player, NPC).
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 struct Fighter {
     max_hp: i32,
     hp: i32,
@@ -328,7 +336,7 @@ struct Fighter {
     on_death: DeathCallback,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 enum DeathCallback {
     Player,
     Monster,
@@ -366,13 +374,14 @@ fn monster_death(monster: &mut Object, game: &mut Game) {
     monster.name = format!("remains of {}", monster.name);
 }
 
-#[derive(Clone, Debug, PartialEq)]
-enum Ai{
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+enum Ai {
     Basic,
     Confused{previous_ai: Box<Ai>, num_turns: i32},
 }
 
 // Generic object: Player, Monster, Item, Stairs
+#[derive(Serialize, Deserialize)]
 struct Object {
     name: String,
     blocks: bool,
@@ -1048,6 +1057,11 @@ fn initialize_fov(map: &Map, tcod: &mut Tcod) {
     tcod.con.clear();       // unexplored area starts black (default bg color)
 }
 
+fn msgbox(text: &str, width: i32, root: &mut Root) {
+    let options: &[&str] = &[];
+    menu(text, options, width, root);
+}
+
 fn new_game(tcod: &mut Tcod) -> (Vec<Object>, Game) {
     // create object representing the player
     let mut player = Object::new(0, 0, '@', "player", colors::WHITE, true);
@@ -1070,6 +1084,21 @@ fn new_game(tcod: &mut Tcod) -> (Vec<Object>, Game) {
     game.log.add("Hi! Tuloy po kayo", colors::RED);
 
     (objects, game)
+}
+
+fn save_game(objects: &[Object], game: &Game) -> Result<(), Box<Error>> {
+    let save_data = serde_json::to_string(&(objects, game))?;
+    let mut file = File::create("savegame")?;
+    file.write_all(save_data.as_bytes())?;
+    Ok(())
+}
+
+fn load_game() -> Result<(Vec<Object>, Game), Box<Error>> {
+    let mut json_save_state = String::new();
+    let mut file = File::open("savegame")?;
+    file.read_to_string(&mut json_save_state)?;
+    let result = serde_json::from_str::<(Vec<Object>, Game)>(&json_save_state)?;
+    Ok(result)
 }
 
 fn play_game(objects: &mut Vec<Object>, game: &mut Game, tcod: &mut Tcod) {
@@ -1097,6 +1126,7 @@ fn play_game(objects: &mut Vec<Object>, game: &mut Game, tcod: &mut Tcod) {
         previous_player_position = objects[PLAYER].pos();
         let player_action = handle_keys(key, tcod, objects, game);
         if player_action == PlayerAction::Exit {
+            save_game(objects, game).unwrap();
             break
         }
 
@@ -1134,6 +1164,18 @@ fn main_menu(tcod: &mut Tcod) {
             Some(0) => {    // new game
                 let (mut objects, mut game) = new_game(tcod);
                 play_game(&mut objects, &mut game, tcod);
+            }
+            Some(1) => {    // load game
+                match load_game() {
+                    Ok((mut objects, mut game)) => {
+                        initialize_fov(&game.map, tcod);
+                        play_game(&mut objects, &mut game, tcod);
+                    }
+                    Err(_e) => {
+                        msgbox("\nNo saved game to load.\n", 24, &mut tcod.root);
+                        continue;
+                    }
+                }
             }
             Some(2) => {    // quit
                 break;
