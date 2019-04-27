76c0b644d9359b5985813eb5cba23c1f6fa052f1
David Rigel Magsipoc <drmagsipoc@gmail.com>
2019-04-27 13:31:16 +0800

    fixed background bug. added main menu

--- src/main.rs
+++ src/main.rs
@@ -413,7 +413,7 @@ impl Object {
 
     /// set the color and then draw the character that represents this object at its position
     pub fn draw(&self, con: &mut Console) {
-        con.set_default_background(self.color);
+        con.set_default_foreground(self.color);
         con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
     }
 
@@ -751,9 +751,13 @@ fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32,
                        root: &mut Root) -> Option<usize> {
     assert!(options.len() <= 26, "Cannot have a menu more than the limit");
 
-    // calculate the total height for the header (after auto-wrap) and one line
+    // calculate total height for the header (after auto-wrap) and one linr
     // per option
-    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let header_height = if header.is_empty() {
+        0
+    } else {
+        root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header)
+    };
     let height = options.len() as i32 + header_height;
 
     // create an off-screen console that represent's the menu;s window
@@ -820,27 +824,27 @@ fn render_all(tcod: &mut Tcod, objects: &[Object], game: &mut Game,
         // recompute FOV if needed (the player moved or something)
         let player = &objects[PLAYER];
         tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
-        for y in 0..MAP_HEIGHT {
-            for x in 0..MAP_WIDTH {
-                let visible = tcod.fov.is_in_fov(x, y);
-                let wall = game.map[x as usize][y as usize].block_sight;
-                let color = match (visible, wall) {
-                    // outside of field of view:
-                    (false, true) => COLOR_DARK_WALL,
-                    (false, false) => COLOR_DARK_GROUND,
-                    // inside fov:
-                    (true, true) => COLOR_LIGHT_WALL,
-                    (true, false) => COLOR_LIGHT_GROUND,
-                };
-                let explored = &mut game.map[x as usize][y as usize].explored;
-                if visible {
-                    // since it's visible, explore it
-                    *explored = true;
-                }
-                if *explored {
-                    // show explored tile only (any visible tile is eplored already)
-                    tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
-                }
+    }
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let visible = tcod.fov.is_in_fov(x, y);
+            let wall = game.map[x as usize][y as usize].block_sight;
+            let color = match (visible, wall) {
+                // outside of field of view:
+                (false, true) => COLOR_DARK_WALL,
+                (false, false) => COLOR_DARK_GROUND,
+                // inside fov:
+                (true, true) => COLOR_LIGHT_WALL,
+                (true, false) => COLOR_LIGHT_GROUND,
+            };
+            let explored = &mut game.map[x as usize][y as usize].explored;
+            if visible {
+                // since it's visible, explore it
+                *explored = true;
+            }
+            if *explored {
+                // show explored tile only (any visible tile is eplored already)
+                tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
     }
@@ -849,26 +853,16 @@ fn render_all(tcod: &mut Tcod, objects: &[Object], game: &mut Game,
     // sort so that non-blocking objects come first
     to_draw.sort_by(|o1, o2| { o1.blocks.cmp(&o2.blocks) });
     // draw the objects in the list
-    for object in to_draw {
+    for object in &to_draw {
         object.draw(&mut tcod.con);
     }
+
     // blit contents of "con" to root console and present it
     blit(&mut tcod.con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), &mut tcod.root, (0, 0), 1.0, 1.0);
 
     // prepare to render the GUI panel
     tcod.panel.set_default_background(colors::BLACK);
     tcod.panel.clear();
-
-    // show the player's stats
-    let hp = objects[PLAYER].fighter.map_or(0, |f| f.hp);
-    let max_hp = objects[PLAYER].fighter.map_or(0, |f| f.max_hp);
-    render_bar(&mut tcod.panel, 1, 1, BAR_WIDTH, "HP", hp, max_hp,
-               colors::LIGHT_RED, colors::DARKER_RED);
-
-    // display the object names under the mouse
-    tcod.panel.set_default_foreground(colors::LIGHT_GREY);
-    tcod.panel.print_ex(1, 0, BackgroundFlag::None, TextAlignment::Left,
-                   get_names_under_mouse(tcod.mouse, objects, &tcod.fov));
 
     // print game messages
     let mut y = MSG_HEIGHT as i32;
@@ -881,6 +875,17 @@ fn render_all(tcod: &mut Tcod, objects: &[Object], game: &mut Game,
         tcod.panel.set_default_foreground(color);
         tcod.panel.print_rect(MSG_X, y, MSG_WIDTH, 0, msg);
     }
+
+    // show the player's stats
+    let hp = objects[PLAYER].fighter.map_or(0, |f| f.hp);
+    let max_hp = objects[PLAYER].fighter.map_or(0, |f| f.max_hp);
+    render_bar(&mut tcod.panel, 1, 1, BAR_WIDTH, "HP", hp, max_hp,
+               colors::LIGHT_RED, colors::DARKER_RED);
+
+    // display the object names under the mouse
+    tcod.panel.set_default_foreground(colors::LIGHT_GREY);
+    tcod.panel.print_ex(1, 0, BackgroundFlag::None, TextAlignment::Left,
+                   get_names_under_mouse(tcod.mouse, objects, &tcod.fov));
 
     // blit contents of "con" to root console and present it
     blit(&mut tcod.panel, (0, 0), (SCREEN_WIDTH, PANEL_HEIGHT), &mut tcod.root,
@@ -1040,6 +1045,7 @@ fn initialize_fov(map: &Map, tcod: &mut Tcod) {
                          !map[x as usize][y as usize].blocked);
         }
     }
+    tcod.con.clear();       // unexplored area starts black (default bg color)
 }
 
 fn new_game(tcod: &mut Tcod) -> (Vec<Object>, Game) {
@@ -1049,7 +1055,7 @@ fn new_game(tcod: &mut Tcod) -> (Vec<Object>, Game) {
     player.fighter = Some(Fighter{max_hp: 8, hp: 8, defense: 1, power: 2,
                                   on_death: DeathCallback::Player});
 
-    // objects list for player
+    // objects list currently populated only by player
     let mut objects = vec![player];
 
     let mut game = Game {
@@ -1082,8 +1088,7 @@ fn play_game(objects: &mut Vec<Object>, game: &mut Game, tcod: &mut Tcod) {
             _ => key = Default::default(),
         }
 
-        let fov_recompute = previous_player_position != (objects[PLAYER].x,
-                                                         objects[PLAYER].y);
+        let fov_recompute = previous_player_position != (objects[PLAYER].pos());
         render_all(tcod, &objects, game, fov_recompute);
 
         tcod.root.flush();
@@ -1106,6 +1111,38 @@ fn play_game(objects: &mut Vec<Object>, game: &mut Game, tcod: &mut Tcod) {
     }
 }
 
+fn main_menu(tcod: &mut Tcod) {
+    let img = tcod::image::Image::from_file("menu_background.png")
+            .ok().expect("Background image not found");
+    while !tcod.root.window_closed() {
+        // show the background image, at twice the regular console resolution
+        tcod::image::blit_2x(&img, (0,0), (-1, -1), &mut tcod.root, (0, 0));
+
+        tcod.root.set_default_foreground(colors::LIGHT_YELLOW);
+        tcod.root.print_ex(SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 4,
+                           BackgroundFlag::None, TextAlignment::Center,
+                           "Bibz Rogue");
+        tcod.root.print_ex(SCREEN_WIDTH/2, SCREEN_HEIGHT - 2,
+                           BackgroundFlag::None, TextAlignment::Center,
+                           "playerz_02");
+
+        // show options and wait for the player's choice
+        let choices = &["Play a new game", "Continue last game", "Quit"];
+        let choice = menu("", choices, 24, &mut tcod.root);
+
+        match choice {
+            Some(0) => {    // new game
+                let (mut objects, mut game) = new_game(tcod);
+                play_game(&mut objects, &mut game, tcod);
+            }
+            Some(2) => {    // quit
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
     let root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -1124,6 +1161,5 @@ fn main() {
         mouse: Default::default(),
     };
 
-    let (mut objects, mut game) = new_game(&mut tcod);
-    play_game(&mut objects, &mut game, &mut tcod);
+    main_menu(&mut tcod);
 }
